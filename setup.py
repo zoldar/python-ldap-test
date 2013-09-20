3f07465eeb17c534470fa5019cc5a36b8b94136b
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2013-09-20 14:43:56 +0200

    Version bump and small description change

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ except:
 
 setup(
     name='python-ldap-test',
-    version='0.0.7',
+    version='0.0.8',
     author='Adrian Gruntkowski',
     author_email='adrian.gruntkowski@gmail.com',
     packages=['ldap_test', 'ldap_test.test'],
     url='https://github.com/zoldar/python-ldap-test/',
     license='LICENSE.txt',
-    description=('Tool for testing code speaking with LDAP. Allows to easily'
+    description=('Tool for testing code speaking with LDAP server. Allows to easily'
                  ' configure and run an embedded, in-memory LDAP server. Uses'
                  ' UnboundID LDAP SDK through Py4J.'),
     keywords = ['testing', 'tests', 'test', 'ldap'],
