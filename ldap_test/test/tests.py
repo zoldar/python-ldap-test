989d374f54f7099ede9dcf16d657e443e6e20bc0
John Kristensen <john@jerrykan.com>
2014-09-19 23:19:54 +1000

    Rework the tests for use with Python3
    Implement the tests as proper TestCases and make use of the python3-ldap
    library which has support for both Python2 and Python3.

--- ldap_test/test/tests.py
+++ ldap_test/test/tests.py
@@ -1,64 +1,82 @@
-#!/usr/bin/env python
-
-import ldap
 import sys
+import unittest
+from os import path
+
+sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), '..', '..')))
+
+import ldap3
 
 from ldap_test import LdapServer
 
 
-if __name__ == "__main__":
-
-    server = LdapServer()
-
-    try:
+class LdapServerTest(unittest.TestCase):
+    def test_default(self):
+        server = LdapServer()
         server.start()
 
         dn = server.config['bind_dn']
         pw = server.config['password']
 
-        con = ldap.initialize('ldap://localhost:%s' % (server.config['port'],))
-        con.simple_bind_s(dn, pw)
+        srv = ldap3.Server('localhost', port=server.config['port'])
+        conn = ldap3.Connection(srv, user=dn, password=pw, auto_bind=True)
 
         base_dn = server.config['base']['dn']
         filter = '(objectclass=domain)'
         attrs = ['dc']
 
-        print con.search_s(base_dn, ldap.SCOPE_SUBTREE, filter, attrs)
+        conn.search(base_dn, filter, attributes=attrs)
 
-    finally:
+        self.assertEqual(conn.response, [{
+            'dn': 'dc=example,dc=com',
+            'raw_attributes': {'dc': [b'example']},
+            'attributes': {'dc': ['example']},
+            'type': 'searchResEntry'
+        }])
+
         server.stop()
 
-    server = LdapServer({
-        'port': 3333,
-        'bind_dn': 'cn=admin,dc=zoldar,dc=net',
-        'password': 'pass1',
-        'base': {'objectclass': ['domain'],
-                 'dn': 'dc=zoldar,dc=net',
-                 'attributes': {'dc': 'zoldar'}},
-        'entries': [
-            {'objectclass': 'domain',
-             'dn': 'dc=users,dc=zoldar,dc=net',
-             'attributes': {'dc': 'users'}},
-            {'objectclass': 'organization',
-             'dn': 'o=foocompany,dc=users,dc=zoldar,dc=net',
-             'attributes': {'o': 'foocompany'}},
-        ]
-    })
-
-    try:
+    def test_config(self):
+        server = LdapServer({
+            'port': 3333,
+            'bind_dn': 'cn=admin,dc=zoldar,dc=net',
+            'password': 'pass1',
+            'base': {
+                'objectclass': ['domain'],
+                'dn': 'dc=zoldar,dc=net',
+                'attributes': {'dc': 'zoldar'}
+            },
+            'entries': [{
+                'objectclass': ['domain'],
+                'dn': 'dc=users,dc=zoldar,dc=net',
+                'attributes': {'dc': 'users'}
+            }, {
+                'objectclass': ['organization'],
+                'dn': 'o=foocompany,dc=users,dc=zoldar,dc=net',
+                'attributes': {'o': 'foocompany'}
+            }],
+        })
         server.start()
 
         dn = "cn=admin,dc=zoldar,dc=net"
         pw = "pass1"
 
-        con = ldap.initialize('ldap://localhost:3333')
-        con.simple_bind_s(dn, pw)
+        srv = ldap3.Server('localhost', port=3333)
+        conn = ldap3.Connection(srv, user=dn, password=pw, auto_bind=True)
 
         base_dn = 'dc=zoldar,dc=net'
-        filter = '(objectclass=domain)'
+        filter = '(objectclass=organization)'
         attrs = ['o']
 
-        print con.search_s(base_dn, ldap.SCOPE_SUBTREE, filter, attrs)
+        conn.search(base_dn, filter, attributes=attrs)
 
-    finally:
+        self.assertEqual(conn.response, [{
+            'dn': 'o=foocompany,dc=users,dc=zoldar,dc=net',
+            'raw_attributes': {'o': [b'foocompany']},
+            'attributes': {'o': ['foocompany']},
+            'type': 'searchResEntry'
+        }])
+
         server.stop()
+
+if __name__ == '__main__':
+    unittest.main()
