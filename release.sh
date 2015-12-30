a5e6c295ba1ec63810cce7575cd52ca301cdc733
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2015-12-30 16:30:21 +0100

    Change release script

---
+++ release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$1" == "prod" ]; then
+    PYPI=pypi
+elif [ "$1" == "test" ]; then
+    PYPI=pypitest
+else
+    echo "Usage: $0 prod|test"
+    exit 1
+fi
+
+/usr/bin/pandoc -f markdown -t rst README.md > README.txt
+
+/usr/bin/python setup.py register -r $PYPI
+
+/bin/rm README.txt
