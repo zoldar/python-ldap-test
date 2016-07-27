2fd6deed2fcfe7164b15d0e71444d4fd4c7aa553
Adrian Gruntkowski <adrian.gruntkowski@gmail.com>
2016-07-27 23:40:17 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except:
 
 setup(
     name='python-ldap-test',
-    version='0.2.1',
+    version='0.2.2',
     author='Adrian Gruntkowski',
     author_email='adrian.gruntkowski@gmail.com',
     packages=['ldap_test', 'ldap_test.test'],
